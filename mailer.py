c25a12f14258ac6773d8cfccd59a720526ef2cac
Avi Gupta <avibilasgupta@gmail.com>
2019-11-14 19:04:21 +0530

    dataframe created of excel sheet table created for mailer

--- mailer.py
+++ mailer.py
@@ -1,7 +1,10 @@
 import argparse
 import smtplib
+import pandas as pd
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+
 from settings import *
 
 def send_mail(to_email, message):
@@ -25,17 +28,70 @@ def mail_arguements(**options):
         to_email = 'avibilasgupta@gmail.com'
     return to_email 
 
+def mail_table():
+    dataframe = pd.read_excel('revenue_sheet.xls')
+    expected_revenue = dataframe.groupby(['Mandi'])['Pickup_Cost', 'Sum_of_Commission'].sum()
+    expected_revenue = expected_revenue.assign(Revenue = expected_revenue['Sum_of_Commission'] - expected_revenue['Pickup_Cost'])
+    sum_expected_revenue = expected_revenue.append(expected_revenue.sum().rename('Total')).round(2).reset_index()
+    return sum_expected_revenue
+
 message = MIMEMultipart()
 
 message['subject'] = 'Automated Mailer Template'
 message['From'] = EMAIL_HOST_USER
+
 to_email = mail_arguements()
 to_email = ', '.join(to_email)
 message['To'] = to_email
 
+revenue_table = mail_table()
 body = 'Dear User, <br/>This mailer is a template for any random automated mailing system.<br/><br/>The table given below is a part of the template for providing with any table format while sending a mail.<br/><br/>'
+row_list = []
+for obj in revenue_table.itertuples(index = False):
 
-body=MIMEText(body, 'html')
+    mandi_row= '''<tr>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                            <td>%s</td>
+                    </tr>'''%(obj.Mandi, obj.Pickup_Cost, obj.Sum_of_Commission, obj.Revenue)
+
+    row_list.append(mandi_row)
+body+= '''
+        <html>
+            <head>
+                <style>
+                    table {
+                        font-family: arial, sans-serif;
+                        border-collapse: collapse;
+                        width: 100;
+                    }
+                    td, th {
+                        border: 1px solid #dddddd;
+                        text-align: left;
+                        padding: 8px;
+                    }
+                    tr:nth-child(even) {
+                        background-color: #dddddd;
+                    }
+                </style>
+            </head>
+            <body>
+                <table>
+                    <tr>
+                        <th>Mandi</th>
+                        <th>Sum of Pickup Cost</th>
+                        <th>Sum of Commission</th>
+                        <th>Expected Revenue</th>
+                    </tr>
+                    <td>%s</td>
+                </table>
+            </body>
+        </html>
+    <br/><br/>Thank you.            
+'''%(''.join(row_list))
+
+body = MIMEText(body, 'html')
 message.attach(body)
 
 try:
